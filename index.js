5685d92be675490db114a32b688ac08236b66b5d
Youssef-C <youssef.chardi@student.kyh.se>
2021-05-07 20:00:49 +0200

    Modal functioning, content in modal next

--- index.js
+++ index.js
@@ -22,11 +22,7 @@ async function getSWAPI() {
     document.getElementById("birth-years").innerHTML = ages;
     document.getElementById("eye-colors").innerHTML =  eyes; */
 
-   /* const modalButton = document.getElementById("modal-open")
-
-    modalButton.onclick = function(){
-        console.log("hello")
-    }; */
+     
 
     nextButton.onclick = function(){
         if (data.next !== null) {
@@ -48,9 +44,44 @@ async function getSWAPI() {
           <h3 class="charTitle">${characters.name}</h3>
           <div class="charAge">Age: ${characters.birth_year}</div>
           <div class="">Eye Color: ${characters.eye_color}</div>
+          <button class="myBtn">Open Modal</button>
         </div>`
-    ).join('')
+    ).join('');
 
+    //---MODAL---
+
+    // Get the modal
+    var modal = document.getElementById("myModal");
+
+    // Get the button that opens the modal
+    var btn = document.getElementsByClassName("myBtn");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on the button, open the modal
+    btn.onclick = function() {
+    modal.style.display = "block";
+    }
+
+    $(".myBtn").click(function() {
+        modal.style.display = "block";
+    });
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+    modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
 };
+
+
+
 
 getSWAPI();
