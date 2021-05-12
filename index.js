eff1cdd285d1cb01c4798c04efb9835f33cb886a
Youssef-C <youssef.chardi@student.kyh.se>
2021-05-12 21:26:45 +0200

    Commented Javascript and CSS, began styling, styling left and search function

--- index.js
+++ index.js
@@ -1,60 +1,36 @@
-const pbody = document.querySelector('pbody');
-
-const nextButton = document.querySelector("#button-next")
-const preButton = document.querySelector("#button-previous")
+const nextButton = document.querySelector("#button-next");
+const preButton = document.querySelector("#button-previous");
 
 const modal = document.getElementById("myModal");
-
-//let SWAPI_url = 'http://swapi.dev/api/people/?page=1';
 
 async function getSWAPI(url = 'http://swapi.dev/api/people/?page=1') {
     const response = await fetch(url);   
     const data = await response.json();
 
-    /*
-    var myObj = (data.results);  
-
-    let names = myObj.map(a => " " + a.name);
-    let ages = myObj.map(a => " " + a.birth_year);
-    let eyes = myObj.map(a => " " + a.eye_color);
-
-    
-    document.getElementById("names").innerHTML = names;
-    document.getElementById("birth-years").innerHTML = ages;
-    document.getElementById("eye-colors").innerHTML =  eyes; */
-
-     
-
+    // These lines (10-21) takes the next and previous button, and when the user clicks them changes the API call to the "next" or "previous" value
     nextButton.onclick = function(){
         if (data.next !== null) { 
             getSWAPI(data.next);
         }
     };
     
-
     preButton.onclick = function(){
         if (data.previous !== null) {
             getSWAPI(data.previous);
         }
-    }
+    };
     
-    document.querySelector('#app').innerHTML = data.results.map(characters => 
-        `<div>
-          <h3 class="charTitle">${characters.name}</h3>
-          <div class="charAge">Age: ${characters.birth_year}</div>
-          <div class="">Eye Color: ${characters.eye_color}</div>
-          <button class="myBtn">Open Modal</button>
+    // These lines print the actual cards and their contents with the data from the API call, appends it into the div called "app" and replaces the placeholders
+    document.querySelector('#flexContainer').innerHTML = data.results.map(characters => 
+        `<div class="charCard">
+            <h3 class="charTitle">${characters.name}</h3>
+            <div class="charAge">Age: ${characters.birth_year}</div>
+            <div class="">Eye Color: ${characters.eye_color}</div>
+            <button class="modalBtn">Open Modal</button>
         </div>`
     ).join('');
 
-    //---Potential solve for the modal button and it's content---
-    var counter = "";
-    for (i = 0; i < data.results.length; i++) {
-            counter += data.results[i].name + "<br>"
-    };
-
-    document.getElementById("demo").innerHTML = counter;
-        
+    // Gets the modal and then maps the data from the API call onto the elements   
     document.querySelector('#modal-content').innerHTML = data.results.map(characters => 
             `<div style="display: none">
                 <span class="close">&times;</span>
@@ -62,23 +38,11 @@ async function getSWAPI(url = 'http://swapi.dev/api/people/?page=1') {
                 <div class="charName">Name: ${characters.name}</div>
             </div>`).join('');
 
+    
+    
     //---MODAL---
 
-    // Get the modal
-    
-
-    // Get the button that opens the modal
-    var btn = document.getElementsByClassName("myBtn");
-
-    // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close");
-
-    // When the user clicks on the button, open the modal
-  /*
-    btn.onclick = function() {
-    modal.style.display = "block";
-    }
-*/
+    //These lines look through all the divs in the modal and, tries to find a match for the name element and then prints data based on the match
     const showPerson = (name) => {
         // scanna samtliga divvar i modalne
         // sätt inline style, display block på den diven som hör till name
@@ -86,45 +50,28 @@ async function getSWAPI(url = 'http://swapi.dev/api/people/?page=1') {
         for (div of modalChildren) {
             const nameContent = div.querySelector('.charName').innerText
             const foundName = nameContent.substr(6, nameContent.length)
+            // Here it looks for a match and if one is found it changes the elements display property
             if (foundName === name) {
                 div.style="display: block"
             }
         }
-    }
-/*
-    const hidePerson = (name) => {
-        // som ovan fast tvärtom
-        const modal = document.querySelector('#modal-content').children
-        for (div of modal) {
-            const nameContent = div.querySelector('.charName').innerText
-            const foundName = nameContent.substr(6, nameContent.length)
-            if (foundName === name) {
-                div.style="display: none"
-            }
-        }
-    }
-*/
+    };
 
-const hideModalContents = () => {
+    // These lines hide the modals children, used later on line 75   
+    const hideModalContents = () => {
     const modalChildren = document.querySelector('#modal-content').children
         for (div of modalChildren) {
             div.style="display: none"
         }
-}
-    $(".myBtn").click(function(event) {
+    };
+    
+    $(".modalBtn").click(function(event) {
         showPerson(event.target.closest('div').children[0].innerText)
         
         modal.style.display = "block";
     });
 
-    // When the user clicks on <span> (x), close the modal
-   /*  $(".close").onclick = function() {
-        console.log("YO")
-    modal.style.display = "none";
-    hideModalContents();
-    } */
-
-    // When the user clicks anywhere outside of the modal, close it
+    // When the user clicks anywhere outside of the modal, or on an element with the class of "close", close the modal
     window.onclick = function(event) {
         if (event.target == modal || event.target.classList.contains('close')) {
             modal.style.display = "none";
@@ -132,8 +79,5 @@ const hideModalContents = () => {
         }
     }
 };
-
-
-
 
 getSWAPI();
